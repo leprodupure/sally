8e94ecd2485965076e6a922c601a0831ba565244
Romaric BREIL <romaric.breil@gmail.com>
2025-09-06 18:24:24 +0200

    fixed ingress rule for db

--- services/core-infra/terraform/database.tf
+++ services/core-infra/terraform/database.tf
@@ -36,26 +36,32 @@ resource "aws_security_group" "db" {
   name        = "${var.project_name}-${var.stack}-${var.module_name}-db-sg"
   description = "Allow PostgreSQL traffic from within the VPC"
   vpc_id      = aws_vpc.main.id
+}
 
-  ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = [aws_vpc.main.cidr_block] # Allows access from any resource in the VPC
-  }
+resource "aws_security_group_rule" "db_ingress" {
+  type              = "ingress"
+  from_port         = 5432
+  to_port           = 5432
+  protocol          = "tcp"
+  cidr_blocks       = [aws_vpc.main.cidr_block]
+  security_group_id = aws_security_group.db.id
+  description       = "Allow PostgreSQL traffic from within the VPC"
+}
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "db_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.db.id
+  description       = "Allow all outbound traffic"
 }
 
 resource "aws_db_instance" "main" {
   identifier             = "${var.project_name}-${var.stack}-${var.module_name}-db"
   engine                 = "postgres"
-  engine_version         = "15"
+  engine_version         = "15.5"
   instance_class         = "db.t3.micro" # Free Tier eligible
   allocated_storage      = 20            # Free Tier eligible
   storage_type           = "gp2"
@@ -64,4 +70,8 @@ resource "aws_db_instance" "main" {
   db_subnet_group_name   = aws_db_subnet_group.main.name
   vpc_security_group_ids = [aws_security_group.db.id]
   skip_final_snapshot    = true
+
+  lifecycle {
+    ignore_changes = [password]
+  }
 }
