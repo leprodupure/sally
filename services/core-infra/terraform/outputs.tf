5affcac6e1a2d6aa8ec233c8ef04151049421a67
Romaric BREIL <romaric.breil@gmail.com>
2025-09-08 12:10:39 +0200

    fix core-infra: removed outputs of values that do not exist anymore

--- services/core-infra/terraform/outputs.tf
+++ services/core-infra/terraform/outputs.tf
@@ -1,18 +1,3 @@
-output "vpc_id" {
-  description = "The ID of the VPC."
-  value       = aws_vpc.main.id
-}
-
-output "private_subnet_ids" {
-  description = "List of IDs of the private subnets."
-  value       = aws_subnet.private[*].id
-}
-
-output "public_subnet_ids" {
-  description = "List of IDs of the public subnets."
-  value       = aws_subnet.public[*].id
-}
-
 output "db_credentials_secret_arn" {
   description = "The ARN of the Secrets Manager secret for DB credentials."
   value       = aws_secretsmanager_secret.db_credentials.arn
