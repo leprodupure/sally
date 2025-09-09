1b61edfb5c84d71d22aca2e2e59bfd6eff43a8d7
Romaric BREIL <romaric.breil@gmail.com>
2025-09-09 18:19:18 +0200

    fixed api gateway version (v2) in core-infra

--- services/core-infra/terraform/outputs.tf
+++ services/core-infra/terraform/outputs.tf
@@ -1,29 +1,19 @@
+output "api_gateway_id" {
+  description = "The ID of the main API Gateway v2 (HTTP API)."
+  value       = aws_apigatewayv2_api.main.id
+}
+
+output "api_gateway_execution_arn" {
+  description = "The execution ARN of the main API Gateway, used for invoking Lambda functions."
+  value       = aws_apigatewayv2_api.main.execution_arn
+}
+
 output "db_credentials_secret_arn" {
-  description = "The ARN of the Secrets Manager secret for DB credentials."
+  description = "The ARN of the secret containing the database credentials."
   value       = aws_secretsmanager_secret.db_credentials.arn
 }
 
 output "db_security_group_id" {
-  description = "The ID of the security group for the RDS database."
+  description = "The ID of the security group for the database."
   value       = aws_security_group.db.id
-}
-
-output "api_gateway_id" {
-  description = "The ID of the core API Gateway."
-  value       = aws_api_gateway_rest_api.main.id
-}
-
-output "api_gateway_root_resource_id" {
-  description = "The ID of the root resource for the core API Gateway."
-  value       = aws_api_gateway_rest_api.main.root_resource_id
-}
-
-output "cognito_user_pool_id" {
-  description = "The ID of the Cognito User Pool."
-  value       = aws_cognito_user_pool.main.id
-}
-
-output "cognito_user_pool_client_id" {
-  description = "The ID of the Cognito User Pool Client."
-  value       = aws_cognito_user_pool_client.main.id
 }
